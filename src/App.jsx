749bb3e4997e4d4394262a659f61f857bcdb09e3
Noelisoa <alainrakotoarivony926@gmail.com>
2025-02-04 23:32:20 +0300

    Fusee animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Bg_etoile from './components/bg_etoile/Bg_etoile'
 import NavBar from './components/header/NavBar'
 import Banniere from './components/header/Banniere'
+import Fusee from './components/Animation/Fusee'
 
 function App() {
 
   return (
-    <div>
+    <div className='app'>
       <NavBar></NavBar>
       <Banniere></Banniere>
       <Bg_etoile></Bg_etoile>
+      <Fusee></Fusee>
     </div>
   )
 }
---
+++ src/components/Animation/Fusee.jsx
@@ -0,0 +1,9 @@
+import "./Fusee.css"
+export default function Fusee() {
+
+  return (
+    <div className="fusee-container">
+      <img src="/Animation_img/rocket.png" className="fusee" alt="Fusée qui décole et accélere" />
+    </div>
+  )
+}
