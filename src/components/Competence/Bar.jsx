1f9715f6fdcf1c172c18768bb2175fbb3073c440
Noelisoa <alainrakotoarivony926@gmail.com>
2025-02-26 21:12:36 +0300

    Diagram v0.2

--- src/components/Competence/Bar.jsx
+++ src/components/Competence/Bar.jsx
@@ -1,10 +1,27 @@
 /* eslint-disable react/prop-types */
-import "./Bar.css"
+import "./Bar.css";
 
-export default function Bar({ color, level}) {
+export default function Bar({ color, level, name }) {
+
   return (
-    <div style={{height: `${level}%`}}>
-        <div style={{background: color, height: "100%", width: "44px", borderRadius: "25px", margin : "auto"}}></div>
+    <div style={{ height: `${level}%` }}>
+      <div
+        style={{
+          background: color,
+          height: "100%",
+          width: "44px",
+          borderRadius: "24px",
+          margin: "auto",
+        }}
+        className="bar"
+
+      >
+        <h2
+          className="tool-name"
+        >
+          {name}
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
